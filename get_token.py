869762d6f8e73e46433d579c713bfeae2fe7389b
Tim Broder <timothy.broder@gmail.com>
2013-02-11 14:57:50 -0500

    comments

--- get_token.py
+++ get_token.py
@@ -4,4 +4,5 @@ import webbrowser
 
 url = 'https://trello.com/1/authorize?key=3e2cd730f3dcccbe15eaf0d39d219a37&name=PythonistaTrello&expiration=never&response_type=token&scope=read,write'
 
+#open web browser to get a permanant Trello API Token
 webbrowser.open(url)
